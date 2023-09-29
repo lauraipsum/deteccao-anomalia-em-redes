b092b92f6cfbbc0bf2d0dacb139405c948ebe533
laura.rodrigues <laura.rodrigues@DESKTOP-7HA34KC>
2023-09-29 09:44:19 -0300

    Correção da função de encontrar vertices de corte

--- main.py
+++ main.py
@@ -11,49 +11,48 @@ def encontrar_vertices_de_corte(lista_de_adjacencia):
     pilha = []  # pilha para implementar a busca em profundidade (DFS)
     vertices_isolados = set(range(len(lista_de_adjacencia)))  # conjunto para armazenar todos os vértices como isolados inicialmente
 
+    def dfs(u, pai):
+        nonlocal tempo
+        momento_descoberta[u] = tempo
+        valor_minimo_alcancavel[u] = tempo
+        tempo += 1
+        filho = 0
+
+        for v in lista_de_adjacencia[u]:
+            if v == pai:
+                continue
+
+            if momento_descoberta[v] == -1:
+                pilha.append((v, u))
+                filho += 1
+                dfs(v, u)
+                valor_minimo_alcancavel[u] = min(valor_minimo_alcancavel[u], valor_minimo_alcancavel[v])
+                
+                if valor_minimo_alcancavel[v] >= momento_descoberta[u] and pai is not None:
+                    vertices_de_corte.add(u)
+            
+            elif v != pai:
+                valor_minimo_alcancavel[u] = min(valor_minimo_alcancavel[u], momento_descoberta[v])
+
+        if filho == 0 and pai is not None:
+            valor_minimo_alcancavel[pai] = min(valor_minimo_alcancavel[pai], valor_minimo_alcancavel[u])
+
     for vertice_inicial in range(len(lista_de_adjacencia)):
-        if momento_descoberta[vertice_inicial] == -1:  # verifica se o vértice de início ainda não foi descoberto
-            pilha.append((vertice_inicial, None))  # se não tiver sido descoberto, adiciona à pilha para iniciar uma nova busca em profundidade
+        if momento_descoberta[vertice_inicial] == -1:
+            dfs(vertice_inicial, None)
 
-            while pilha:
-                u, pai = pilha[-1]  # pega o vértice no topo da pilha e seu vértice pai
-                if momento_descoberta[u] == -1:
-                    momento_descoberta[u] = tempo  # registra o tempo de descoberta para o vértice
-                    valor_minimo_alcancavel[u] = tempo
-                    tempo += 1
-
-                filho = 0
-                for v in lista_de_adjacencia[u]:
-                    if v == pai:
-                        continue
-
-                    if momento_descoberta[v] == -1:
-                        pilha.append((v, u))  # adiciona os vizinhos não visitados à pilha
-                        filho += 1
-
-                    valor_minimo_alcancavel[u] = min(valor_minimo_alcancavel[u], momento_descoberta[v])  # atualiza o valor "valor_minimo_alcancavel" do vértice atual
-
-                if filho == 0 and pai is not None:
-                    valor_minimo_alcancavel[pai] = min(valor_minimo_alcancavel[pai], valor_minimo_alcancavel[u])  # atualiza o valor "valor_minimo_alcancavel" do pai (se necessário)
-
-                todos_filhos_visitados = all(momento_descoberta[v] != -1 for v in lista_de_adjacencia[u])
-                if todos_filhos_visitados:
-                    pilha.pop()  # remove o vértice atual da pilha se todos os vizinhos foram visitados
-
-                if todos_filhos_visitados and pai is not None and valor_minimo_alcancavel[u] >= momento_descoberta[u]:
-                    vertices_de_corte.add(pai)  # se a condição for atendida, o vértice pai é um vértice de corte
-
-    # remove os vértices de corte do conjunto de vértices isolados
     for vertex in vertices_de_corte:
         vertices_isolados.discard(vertex)
 
     with open("VerticesDeCorte.txt", "w") as f_vertices_de_corte:
         for vertex in vertices_de_corte:
             f_vertices_de_corte.write(f"{vertex}\n")
+            print(f"Vértice de corte: {vertex}")
 
     with open("VerticesIsolados.txt", "w") as f_vertices_isolados:
         for vertex in vertices_isolados:
             f_vertices_isolados.write(f"{vertex}\n")
 
 print("Iniciando a análise do grafo...")
+print(lista_de_adjacencia)
 encontrar_vertices_de_corte(lista_de_adjacencia)
