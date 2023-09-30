8ccfea28d9f39b235bee09fbe128747eeec5a14c
Myngruan <77254649+Myngruan@users.noreply.github.com>
2023-09-30 02:25:15 -0300

    mudar o grafo para direcionado

--- main.py
+++ main.py
@@ -43,9 +43,13 @@ def encontrar_vertices_de_corte(lista_de_adjacencia):
         if momento_descoberta[vertice_inicial] == -1:
             dfs(vertice_inicial, None)
 
+
+    '''
     for vertex in vertices_de_corte:
         vertices_isolados.discard(vertex)
-
+        print(vertices_isolados)
+    '''
+    
     vertices_de_corte_list = list(vertices_de_corte)
 
     with open("VerticesDeCorte.txt", "w") as f_vertices_de_corte:
@@ -53,21 +57,41 @@ def encontrar_vertices_de_corte(lista_de_adjacencia):
             f_vertices_de_corte.write(f"{vertex}\n")
 
     with open("VerticesIsolados.txt", "w") as f_vertices_isolados:
+        
+        print(lista_de_adjacencia)
+        print(vertices_isolados)
+        
         for vertex in vertices_de_corte_list:
             f_vertices_isolados.write(f'"{vertex}": [\n')
 
             isolados = [v for v in vertices_isolados if v != vertex]
+            isolados_sets = []
 
-            for i, isolado in enumerate(isolados):
-                f_vertices_isolados.write(f'    {isolado}')
-                if i < len(isolados) - 1:
+            for isolado in isolados:
+                isolados_sets.append({isolado})
+
+            while isolados_sets:
+                combined_sets = set()
+                for s in isolados_sets:
+                    combined_sets |= s
+                    isolados_sets.remove(s)
+
+                f_vertices_isolados.write(f'    {combined_sets}')
+                if isolados_sets:
                     f_vertices_isolados.write(',')
+
                 f_vertices_isolados.write('\n')
 
             f_vertices_isolados.write(']\n')
 
-    
-    G = nx.Graph()
+
+    G = nx.DiGraph()  # Crie um grafo direcionado
+
+    # Adicione nós ao grafo
+    for i in range(len(lista_de_adjacencia)):
+        G.add_node(i)
+
+    # Adicione arestas direcionadas com base nas informações da lista de adjacência
     for u, neighbors in enumerate(lista_de_adjacencia):
         for v in neighbors:
             G.add_edge(u, v)
@@ -78,7 +102,6 @@ def encontrar_vertices_de_corte(lista_de_adjacencia):
     nx.draw(G, pos, with_labels=True, node_color=node_colors, node_size=500, font_size=8)
     plt.title('Graph Visualization')
     plt.show()
-
     
 
 print("Iniciando a análise do grafo...")
