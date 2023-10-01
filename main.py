f5c107b4f601fdfe9498428a7b78073a7b327a17
Laura <lauracardoso_1@outlook.com>
2023-09-30 21:52:15 -0300

    Subgrafos gerados e seus componentes + Grafo plotado usando network x

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import networkx as nx
+import matplotlib.pyplot as plt
 from data.data_read import criar_lista_de_adjacencia
 
 lista_de_adjacencia = criar_lista_de_adjacencia()
@@ -27,10 +29,10 @@ def encontrar_vertices_de_corte(lista_de_adjacencia):
                 filho += 1
                 dfs(v, u)
                 valor_minimo_alcancavel[u] = min(valor_minimo_alcancavel[u], valor_minimo_alcancavel[v])
-                
+
                 if valor_minimo_alcancavel[v] >= momento_descoberta[u] and pai is not None:
                     vertices_de_corte.add(u)
-            
+
             elif v != pai:
                 valor_minimo_alcancavel[u] = min(valor_minimo_alcancavel[u], momento_descoberta[v])
 
@@ -41,18 +43,84 @@ def encontrar_vertices_de_corte(lista_de_adjacencia):
         if momento_descoberta[vertice_inicial] == -1:
             dfs(vertice_inicial, None)
 
-    for vertex in vertices_de_corte:
-        vertices_isolados.discard(vertex)
-
     with open("VerticesDeCorte.txt", "w") as f_vertices_de_corte:
         for vertex in vertices_de_corte:
             f_vertices_de_corte.write(f"{vertex}\n")
             print(f"Vértice de corte: {vertex}")
 
-    with open("VerticesIsolados.txt", "w") as f_vertices_isolados:
-        for vertex in vertices_isolados:
-            f_vertices_isolados.write(f"{vertex}\n")
+    return vertices_de_corte
+
+def encontrar_subgrafos_apos_remocao(vertices_de_corte, lista_de_adjacencia):
+    subgrafos = []
+
+    for vertice_de_corte in vertices_de_corte:
+       
+        grafo_temporario = [lista.copy() for lista in lista_de_adjacencia]
+        
+        grafo_temporario[vertice_de_corte] = []
+        for i in range(len(grafo_temporario)):
+            grafo_temporario[i] = [v for v in grafo_temporario[i] if v != vertice_de_corte]
+
+        componentes_conectados = []
+        visitados = [False] * len(grafo_temporario)
+
+        def dfs(u):
+            visitados[u] = True
+            componente_conectado.append(u)
+            for v in grafo_temporario[u]:
+                if not visitados[v]:
+                    dfs(v)
+
+        for vertice in range(len(grafo_temporario)):
+            if not visitados[vertice]:
+                componente_conectado = []
+                if vertice != vertice_de_corte:  # evita adicionar o vértice removido ao componente
+                    dfs(vertice)
+                    componentes_conectados.append(componente_conectado)
+
+        subgrafos.append((vertice_de_corte, componentes_conectados))
+
+    return subgrafos
+
 
 print("Iniciando a análise do grafo...")
 print(lista_de_adjacencia)
-encontrar_vertices_de_corte(lista_de_adjacencia)
+vertices_de_corte = encontrar_vertices_de_corte(lista_de_adjacencia)
+
+subgrafos = encontrar_subgrafos_apos_remocao(vertices_de_corte, lista_de_adjacencia)
+
+G = nx.Graph()
+for u, neighbors in enumerate(lista_de_adjacencia):
+    for v in neighbors:
+        G.add_edge(u, v)
+
+
+plt.figure(figsize=(8, 6))
+pos = nx.spring_layout(G, seed=42) 
+nx.draw(G, pos, with_labels=True, node_size=200, node_color='lightblue', font_size=10, font_color='black')
+plt.title("Grafo original")
+plt.show(block=True) 
+
+for i, (vertice_de_corte, subgrafo) in enumerate(subgrafos):
+    print(f"Subgrafo {i + 1} (removendo vértice de corte {vertice_de_corte}):")
+    for j, componente in enumerate(subgrafo):
+        print(f"Componente {j + 1}:", componente)
+        
+        subgraph_G = G.subgraph(componente) 
+        plt.figure(figsize=(8, 6))
+        nx.draw(
+            subgraph_G,
+            pos,
+            with_labels=True,
+            node_size=200,
+            node_color='lightblue',
+            font_size=10,
+            font_color='black'
+        )
+        plt.title(f"Subgraph {i + 1} - Component {j + 1}")
+        plt.show(block=True) 
+
+plt.show()
+
+
+
